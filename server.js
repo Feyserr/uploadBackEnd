b26cf4e53beec7c7621baba78d33d80aaa92a9ed
Feyserr <51207246+Feyserr@users.noreply.github.com>
2021-07-28 17:26:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-require("dotenv").config();
-const express = require('express'); 
+const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
-
 
 app.use(express.urlencoded({
     extended: false
   }));
 app.use(express.json({ type: 'application/json' }));
 app.use(cors());
-app.use('/', require('./server/route/funcionariosRoute'));
+app.use('/', require('./server/route/productsRoute'));
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server running");
-});
-app.use(bodyParser.urlencoded({extended:true}));
+app.listen(3000,()=>{
+    console.log('Start');
+})
